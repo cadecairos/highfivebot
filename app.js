d321122ffde9ddd51d55428d0ec74b00c9d63643
Christopher De Cairos <christopherd@mozillafoundation.org>
2013-08-07 10:54:03 -0400

    More messages, more regexes

--- app.js
+++ app.js
@@ -4,9 +4,11 @@
 
 var Habitat = require( "habitat" ),
     irc = require( "irc" ),
+    responseGenerator = require( "./lib/responses" )(),
     env,
     client,
-    hi5regex;
+    nick,
+    hi5regexes;
 
 Habitat.load();
 env = (new Habitat()).all();
@@ -15,7 +17,15 @@ if ( !env.HOST, !env.NICK, !env.CHANNELS ) {
   throw new Error( "Missing config options, check your config!" );
 }
 
-hi5regex = new RegExp( "^" + env.NICK + ": o\/$" );
+nick = env.NICK;
+
+hi5regexes = [
+  new RegExp( "^" + nick + ":?\\s*o\\/\\s*$" ),
+  new RegExp( "^" + nick + ":?\\s*\\\\o\\/\\s*$" ),
+  new RegExp( "^" + nick + ":?\\s*wo+[!1]*\\s*$" )
+];
+
+hi5regexes.forEach(function(s){ console.log( s );});
 
 client = new irc.Client( env.HOST, env.NICK, {
   channels: env.CHANNELS.split( "," ).map(function( channel ) {
@@ -27,11 +37,16 @@ client = new irc.Client( env.HOST, env.NICK, {
 });
 
 client.addListener( "message", function( from, to, message ) {
-  if ( hi5regex.test( message ) ) {
-    client.say( to, "\\o You Rock, " + from + "!!!" );
-  }
+  var responded = false;
+  hi5regexes.forEach(function( regex ) {
+    console.log( regex, regex.test( message ) );
+    if ( !responded && regex.test( message ) ) {
+      responded = true;
+      client.say( to, responseGenerator( from ) );
+    }
+  });
 });
 
-client.addListener('error', function(message) {
-    console.log('error: ', message);
+client.addListener( "error", function( message ) {
+    console.log( "error: ", message );
 });
